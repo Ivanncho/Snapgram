2976fc56ea892619ab6adb57da67752e40ff76ef
Ivancho Jovanovski <jovanovski.ivanco@yahoo.com>
2023-11-29 07:03:23 +0100

    initial commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,26 @@
+import * as z from 'zod';
+
+export const SignupValidationSchema = z.object({
+    name: z.string().min(2,{
+        message: "Too short name. At least 2 characters"
+    }),
+    username: z.string().min(2, {
+        message: "Too short username. At least 2 characters"
+    }),
+    email: z.string().email(),
+    password: z.string().min(8, {
+        message:"Too short password. At least 8 characters"
+    }),
+})
+export const SigninValidationSchema = z.object({
+    email: z.string().email(),
+    password: z.string().min(8, {
+        message:"Too short password. At least 8 characters"
+    }),
+})
+export const PostValidationSchema = z.object({
+    caption: z.string().min(5).max(2200),
+    file: z.custom<File[]>(),
+    location: z.string().min(2).max(100),
+    tags: z.string(),
+})
